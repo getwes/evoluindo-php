a1d276430132ee1712570e268ce6c06793213d3a
getwes <wesleyfreire1707@gmail.com>
2021-05-31 20:58:22 -0300

    add teste

---
+++ teste.php
@@ -0,0 +1,11 @@
+<?php
+
+$input = array("Neo", "Morpheus", "Trinity", "Cypher", "Tank","wally",);
+$rand_keys = $input;
+echo $rand_keys[0] . "\n";
+echo $rand_keys[1] . "\n";
+echo $rand_keys[2] . "\n";
+echo $rand_keys[3] . "\n";
+echo $rand_keys[4] . "\n";
+echo $rand_keys[5] . "\n";
+?>
