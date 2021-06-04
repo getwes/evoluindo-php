1d30fe6ccda6f79fbb83ca585af284162843c7e9
getwes <wesleyfreire1707@gmail.com>
2021-06-03 23:02:02 -0300

    atualização

--- banco.php
+++ banco.php
@@ -1,28 +1,6 @@
 <?php
 
-function exibeMensagem($mensagem){
-    echo $mensagem . PHP_EOL;
-}
-
-function sacar(array $conta, float $valorASacar): array
-{
-    if ($valorASacar > $conta['saldo']) {
-        exibeMensagem("Você não pode sacar este valor");
-    } else {
-        $conta['saldo'] -= $valorASacar;
-    }
-
-    return $conta;
-}
-function depositar(array $conta, float $valorADepositar): array {
-    if ($valorADepositar > 0) {
-        $conta['saldo'] += $valorADepositar;
-    } else {
-        exibeMensagem("Depósitos precisam ser positivos");
-    }
-
-    return $conta;
-}
+include 'funcoes.php';
 
 $contasCorrentes = [
     '123.456.789-10' => [
@@ -59,8 +37,6 @@ $contasCorrentes['123.456.789-11'] = sacar(
     $contasCorrentes['123.456.789-11'], 
     200
 );
-foreach ($contasCorrentes as $cpf => $conta) {
-
     foreach ($contasCorrentes as $cpf => $conta) {
         exibeMensagem(
             "$cpf {$conta['titular']} {$conta['saldo']}"
